d6723e6bc687a5062f41ad1c89ce37522cb5b953
threatthreat <grezy8007@gmail.com>
2025-05-02 11:49:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -233,11 +233,15 @@ async def start(client, message):
                     howtodownload = settings.get('tutorial_3', TUTORIAL_3)
                 else:
                     howtodownload = settings.get('tutorial_2', TUTORIAL_2) if is_second_shortener else settings.get('tutorial', TUTORIAL)
-                buttons = [[
-                    InlineKeyboardButton(text="♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=verify)
-                ],[
-                    InlineKeyboardButton(text="⁉️ ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ ⁉️", url=howtodownload)
-                ]]
+                buttons = [
+    [
+        InlineKeyboardButton(text="✅ VERIFY ✅", url=verify),
+        InlineKeyboardButton(text="❓ HOW TO VERIFY ❓", url=howtodownload)
+    ],
+    [
+        InlineKeyboardButton(text="😄 BUY SUBSCRIPTION - NO NEED TO VERIFY 😄", url=f"https://t.me/{temp.U_NAME}?start=plan")
+    ]
+]
                 reply_markup=InlineKeyboardMarkup(buttons)
                 if await db.user_verified(user_id): 
                     msg = script.THIRDT_VERIFICATION_TEXT
